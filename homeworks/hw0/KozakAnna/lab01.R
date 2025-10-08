1bea366d321029b2fa80bfc6ee1a9f4ff46093c3
Anna Kozak <anna1993kozak@gmail.com>
2025-10-08 11:30:26 +0200

    Kozak Anna [PD0]

---
+++ homeworks/hw0/KozakAnna/lab01.R
@@ -0,0 +1,24 @@
+install.packages("proton")
+library(proton)
+
+proton()
+
+
+if(WARUNEK){
+  JEŻELI PRAWDA
+  
+  
+}else{
+  JEŻELI FAŁSZ
+  
+}
+
+for(i in 1:X){
+
+  WYKONUJ
+
+}
+
+X - LICZBA LUB WEKTOR
+
+data.frame(table(XXX))
